9689deb37e228be08c325632c6160177e0da08b8
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-02 22:47:18 +0700

    update function create data phone

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -5,15 +5,58 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\ProductModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Ramsey\Uuid\Uuid;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function getAllData()
     {
         $data = ProductModel::with('detail')->get();
         return response()->json([
             'code' => 200,
             'message' => 'success',
+            'data' => $data
+        ]);
+    }
+
+    public function createData(Request $request)
+    {
+        $validation = Validator::make($request->all(),[
+            'product_name' => 'required',
+            'product_model' => 'required',
+            'price' => 'required' ,
+            'stock' => 'required'
+        ]);
+
+        if ($validation->fails()) {
+            return response()->json([
+                'code' => 401,
+                'message' => 'check your validation',
+                'errors' => $validation->errors()
+            ]);
+        }
+
+        try {
+            $data = new ProductModel();
+            $data->uuid = Uuid::uuid4()->toString();
+            $data->product_name = $request->input('product_name');
+            $data->product_model = $request->input('product_model');
+            $data->price = $request->input('price');
+            $data->stock = $request->input('stock');
+            $data->detail_id = $request->input('detail_id');
+            $data->save();
+        } catch (\Throwable $th) {
+            return response()->json([
+                'code' => 402,
+                'message' => 'failed input data phone',
+                'errors' => $th->getMessage()
+            ]);
+        }
+
+        return response()->json([
+            'code' => 200,
+            'message' => 'success create data',
             'data' => $data
         ]);
     }
