e51732146733a45823b080922c9e80465a0b4905
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-04 10:24:42 +0700

    update ui

--- resources/views/layoutsfe/navbar.blade.php
+++ resources/views/layoutsfe/navbar.blade.php
@@ -8,44 +8,49 @@
                 <div class="full">
                    <div class="center-desk">
                       <div class="logo">
-                         <a href="index.html"><img src="images/ibox_logo.png" class="w-25" alt="#" /></a>
+                         <a href="index.html"><img src="images/ibox_logo.svg" class="w-25" alt="#" /></a>
                       </div>
                    </div>
                 </div>
              </div>
              <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                <nav class="navigation navbar navbar-expand-md navbar-dark ">
+                <nav class="navbar navbar-expand-md navbar-dark ">
                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                    <span class="navbar-toggler-icon"></span>
                    </button>
                    <div class="collapse navbar-collapse" id="navbarsExample04">
-                      <ul class="navbar-nav mr-auto">
+                      <ul class="navbar-nav mr-auto font-weight-bold">
                          <li class="nav-item active">
-                            <a class="nav-link" href="index.html">Home</a>
+                            <a class="nav-link mx-3" href="index.html">Home</a>
                          </li>
                          <li class="nav-item">
-                            <a class="nav-link" href="about.html">About</a>
+                            <a class="nav-link mx-3" href="about.html">About</a>
                          </li>
                          <li class="nav-item">
-                            <a class="nav-link" href="glasses.html">Our Glasses</a>
+                            <a class="nav-link mx-3" href="glasses.html">Our Glasses</a>
                          </li>
                          <li class="nav-item">
-                            <a class="nav-link" href="shop.html">Shop</a>
+                            <a class="nav-link mx-3" href="shop.html">Shop</a>
                          </li>
                          <li class="nav-item">
-                            <a class="nav-link" href="contact.html">Contact Us</a>
-                         </li>
-                         <li class="nav-item d_none login_btn">
-                            <a class="nav-link" href="#">Login</a>
-                         </li>
-                         <li class="nav-item d_none">
-                            <a class="nav-link" href="#">Register</a>
+                            <a class="nav-link mx-3" href="contact.html">Contact Us</a>
                          </li>
                          <li class="nav-item d_none sea_icon">
-                            <a class="nav-link" href="#"><i class="fa fa-shopping-bag" aria-hidden="true"></i><i class="fa fa-search" aria-hidden="true"></i></a>
+                            <a class="nav-link mx-3" href="#"><i class="fa fa-shopping-bag" aria-hidden="true"></i><i class="fa fa-search" aria-hidden="true"></i></a>
                          </li>
                       </ul>
                    </div>
+                   <div class="collapse navbar-collapse" id="navbarExample04">
+                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item d_none btn btn-primary p-0">
+                           <a class="nav-link mx-3" href="#">Login</a>
+                        </li>
+                        <li class="nav-item d_none">
+                           <a class="nav-link mx-3" href="#">Register</a>
+                        </li>
+                     </ul>
+                  </div>
+                  
                 </nav>
              </div>
           </div>
