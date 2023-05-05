68e42cf34e336a4c9cc83e85b2a889a46b1a2015
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-05 20:20:18 +0700

    edit ui nav

--- resources/views/layoutsfe/navbar.blade.php
+++ resources/views/layoutsfe/navbar.blade.php
@@ -1,62 +1,80 @@
    <!-- header -->
    <header>
-    <!-- header inner -->
-    <div class="header">
-       <div class="container-fluid">
-          <div class="row">
-             <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
-                <div class="full">
-                   <div class="center-desk">
-                      <div class="logo">
-                         <a href="index.html"><img src="images/ibox_logo.svg" class="w-25" alt="#" /></a>
-                      </div>
+       <!-- header inner -->
+       <div class="header">
+           <div class="container-fluid fixed-top">
+               <div class="row">
+                   <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                       <div class="full">
+                           <div class="center-desk">
+                               <div class="logo">
+                                   <a href="index.html"><img src="images/ibox_logo.svg" class="w-25"
+                                           alt="#" /></a>
+                               </div>
+                           </div>
+                       </div>
                    </div>
-                </div>
-             </div>
-             <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                <nav class="navbar navbar-expand-md navbar-dark ">
-                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                   <span class="navbar-toggler-icon"></span>
-                   </button>
-                   <div class="collapse navbar-collapse" id="navbarsExample04">
-                      <ul class="navbar-nav mr-auto font-weight-bold">
-                         <li class="nav-item active">
-                            <a class="nav-link mx-3" href="index.html">Home</a>
-                         </li>
-                         <li class="nav-item">
-                            <a class="nav-link mx-3" href="about.html">About</a>
-                         </li>
-                         <li class="nav-item">
-                            <a class="nav-link mx-3" href="glasses.html">Our Glasses</a>
-                         </li>
-                         <li class="nav-item">
-                            <a class="nav-link mx-3" href="shop.html">Shop</a>
-                         </li>
-                         <li class="nav-item">
-                            <a class="nav-link mx-3" href="contact.html">Contact Us</a>
-                         </li>
-                         <li class="nav-item d_none sea_icon">
-                            <a class="nav-link mx-3" href="#"><i class="fa fa-shopping-bag" aria-hidden="true"></i><i class="fa fa-search" aria-hidden="true"></i></a>
-                         </li>
-                      </ul>
-                   </div>
-                   <div class="collapse navbar-collapse" id="navbarExample04">
-                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item d_none btn btn-primary p-0">
-                           <a class="nav-link mx-3" href="#">Login</a>
-                        </li>
-                        <li class="nav-item d_none">
-                           <a class="nav-link mx-3" href="#">Register</a>
-                        </li>
-                     </ul>
-                  </div>
-                  
-                </nav>
-             </div>
-          </div>
-       </div>
-    </div>
- </header>
+                   <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                       <nav class="navbar navbar-expand-md navbar-dark ">
+                           <button class="navbar-toggler" type="button" data-toggle="collapse"
+                               data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false"
+                               aria-label="Toggle navigation">
+                               <span class="navbar-toggler-icon"></span>
+                           </button>
+                           <div class="collapse navbar-collapse" id="navbarsExample04">
+                               <ul class="navbar-nav mr-auto font-weight-bold">
+                                   <li class="nav-item active">
+                                       <a class="nav-link mx-3" href="index.html">Home</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link mx-3" href="about.html">About</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link mx-3" href="glasses.html">Our Glasses</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link mx-3" href="shop.html">Shop</a>
+                                   </li>
+                                   <li class="nav-item">
+                                       <a class="nav-link mx-3" href="contact.html">Contact Us</a>
+                                   </li>
+                                   <li class="nav-item d_none sea_icon">
+                                       <a class="nav-link mx-3" href="#"><i class="fa fa-shopping-bag"
+                                               aria-hidden="true"></i><i class="fa fa-search"
+                                               aria-hidden="true"></i></a>
+                                   </li>
+                               </ul>
+                           </div>
+                           <div class="collapse navbar-collapse" id="navbarExample04">
+                               <ul class="navbar-nav ml-auto">
+                                   <li class="nav-item d_none btn btn-primary p-0">
+                                       <a class="nav-link mx-3" href="#">Login</a>
+                                   </li>
+                                   <li class="nav-item d_none">
+                                       <a class="nav-link mx-3" href="#">Register</a>
+                                   </li>
+                               </ul>
+                           </div>
 
- <!-- end header inner -->
- <!-- end header -->
+                       </nav>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </header>
+
+   <script>
+       $(window).scroll(function() {
+           var scrollTop = $(this).scrollTop();
+           if (scrollTop > 50) {
+               $('.navbar').addClass('scrolled');
+           } else {
+               $('.navbar').removeClass('scrolled');
+           }
+       });
+   </script>
+
+
+
+   <!-- end header inner -->
+   <!-- end header -->
