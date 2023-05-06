704b02ee5ff7f3ffe500998d06e74679a5e99d99
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-06 11:29:24 +0700

    update ui

---
+++ resources/views/backend/Phone.blade.php
@@ -0,0 +1,88 @@
+@extends('layoutsbe.Base')
+@section('content')
+    <div class="col-lg-12">
+        <div class="card mb-4">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Data Artikel</h6>
+               <a href="/create/artikel" class="btn btn-primary">Tambah Data</a>
+            </div>
+            <div class="p-3">
+                <div class="row" id="data-container">
+                    <div class="table-responsive p-3">
+                        <table id="dataTable" class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>No</th>
+                                    <th>Uuid</th>
+                                    <th>Product Name</th>
+                                    <th>Product Model</th>
+                                    <th>Price</th>
+                                    <th>Stock</th>
+                                    <th>Product Image</th>
+                                    <th>Detail ID</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!-- Data from database will be shown here -->
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+ 
+
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script>
+        //get data
+        $(document).ready(function() {
+            $.ajax({
+                url: "{{ url('v1/phone') }}",
+                method: "GET",
+                dataType: "json",
+                success: function(response) {
+                    console.log(response);
+                    var tableBody = "";
+                    $.each(response.data, function(index, item) {
+                        tableBody += "<tr>";
+                        tableBody += "<td>" + (index + 1) + "</td>";
+                        tableBody += "<td>" + item.uuid+ "</td>";
+                        tableBody += "<td>" + item.product_name+ "</td>";
+                        tableBody += "<td>" + item.product_model+ "</td>";
+                        tableBody += "<td>" + item.price.toLocaleString('id') + "</td>";
+                        tableBody += "<td>" + item.stock+ "</td>";
+                        tableBody += "<td><img src='/uploads/phone/" + item.image_phone +
+                            "' alt='" +
+                            item.title +
+                            "' class='img-thumbnail' style='width: 200px'></td>";
+                            tableBody += "<td>" + item.detail_id+ "</td>";
+                        tableBody += "<td>" +
+                            "<button type='button' class='btn btn-primary edit-modal' data-toggle='modal' data-target='#EditModal' " +
+                            "data-id='" + item.id + "' " +
+                            "<i class='fa fa-edit'>Edit</i></button>" +
+                            "<button type='button' class='btn btn-danger delete-confirm' data-id='" +
+                            item.id + "'><i class='fa fa-trash'></i></button>" +
+                            "</td>";
+
+                        tableBody += "</tr>";
+                    });
+                    $('#dataTable').DataTable().destroy();
+                    $("#dataTable tbody").empty();
+                    $("#dataTable tbody").append(tableBody);
+                    $('#dataTable').DataTable({
+                        "paging": true,
+                        "ordering": true,
+                        "searching": true
+                    });
+                },
+                error: function() {
+                    console.log("Failed to get data from server");
+                }
+            });
+        });
+    </script>
+@endsection
