9df8c1329d10e306878923022d59081a15251e0d
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-09 22:21:45 +0700

    update url

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\API\DetailProductController;
+use App\Http\Controllers\API\OrderanController;
 use App\Http\Controllers\API\ProductController;
+use App\Models\OrderanModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +22,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('v3')->controller(OrderanController::class)->group(function () {
+    Route::get('/order', 'getAllData');
+    Route::post('/phone/order', 'createOrderan');
+    Route::get('/order/get/{id}','getDataByid');
+    Route::put('/detail/update/{uuid}',  'updateData');
+});
