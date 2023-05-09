4d8d9c0032df94d5150fe92b13ac0bedaad7a65c
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-09 22:20:12 +0700

    create function migration

---
+++ database/migrations/2023_05_09_130121_create_tb_orderan_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_orderan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('tb_product');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('phone_number');
+            $table->string('post_code');
+            $table->string('city');
+            $table->string('detail_state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_orderan');
+    }
+};
