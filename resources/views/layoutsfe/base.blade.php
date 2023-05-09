16238138f7d9f4839f94737576dbd01568bf7fde
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-09 15:20:21 +0700

    update base

--- resources/views/layoutsfe/base.blade.php
+++ resources/views/layoutsfe/base.blade.php
@@ -34,9 +34,9 @@
 <!-- body -->
 <body class="main-layout">
     <!-- loader  -->
-    <div class="loader_bg">
+    {{-- <div class="loader_bg">
         <div class="loader"><img src="{{ asset('images/loading.gif') }}" alt="#" /></div>
-    </div>
+    </div> --}}
     <!-- end loader -->
     @include('layoutsfe.navbar')
     <!-- banner -->
