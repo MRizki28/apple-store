fd7517e62c5517a55e7d1c894517f272fcb452dd
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-09 22:21:04 +0700

    update ui detail product and creat jquery handle api

--- resources/views/frondend/detail.blade.php
+++ resources/views/frondend/detail.blade.php
@@ -8,9 +8,7 @@
             </div>
             <div class="col-md-12  " style="margin-bottom: 120px;">
                 <div class="card">
-                    <div class="card-header">
-                        <input type="hidden" name="menu_id" id="menu_id" value="">
-                    </div>
+
                     <div class="card-body">
                         <div class="row">
                             <div class="col-md-6">
@@ -19,60 +17,66 @@
                                 <div>
                                     <table class="table">
                                         <tbody class="text-center">
-                                           
+
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
                             <div class="col-md-6 mt-4">
-                                
                                 <table class="table">
-                                    <form method="post" action="">
-                                        @csrf
+                                    <tbody>
+                                        <tr>
+                                            <td>Ram</td>
+                                            <td>:</td>
+                                            <td id="ram"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Storage</td>
+                                            <td>:</td>
+                                            <td id="storage"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>OS</td>
+                                            <td>:</td>
+                                            <td id="os"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Cpu</td>
+                                            <td>:</td>
+                                            <td id="cpu"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Battery</td>
+                                            <td>:</td>
+                                            <td id="baterry"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Camera</td>
+                                            <td>:</td>
+                                            <td id="camera"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Harga</td>
+                                            <td>:</td>
+                                            <td class="" id="price">Rp. </td>
+
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                                <form method="post" action="" id="formTambah">
+                                    @csrf
+                                    <table class="table">
+
+
+                                        <input type="hidden" name="product_id" id="product_id">
                                         <tbody>
-                                            <h2 class="d-flex justify-content-center ">Specification :</h2>
-                                            <tr>
-                                                <td>Ram</td>
-                                                <td>:</td>
-                                                <td id="ram"></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Storage</td>
-                                                <td>:</td>
-                                                <td id="storage"></td>
-                                            </tr>
-                                            <tr>
-                                                <td>OS</td>
-                                                <td>:</td>
-                                                <td id="os"></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Cpu</td>
-                                                <td>:</td>
-                                                <td id="cpu"></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Battery</td>
-                                                <td>:</td>
-                                                <td id="baterry"></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Camera</td>
-                                                <td>:</td>
-                                                <td id="camera"></td>
-                                            </tr>
-                                            <tr>
-                                                <td>Harga</td>
-                                                <td>:</td>
-                                                <td class="" id="price">Rp. </td>
-                                                <input type="hidden" name="harga" id="harga">
-                                            </tr>
-                                           
+
                                             <tr>
                                                 <td>First Name</td>
                                                 <td>:</td>
                                                 <td>
-                                                    <input type="text" name="first_name" id="first_name"
+                                                    <input type="text" name="firstname" id="first_name"
                                                         class="form-control">
                                                 </td>
 
@@ -81,7 +85,7 @@
                                                 <td>Last Name</td>
                                                 <td>:</td>
                                                 <td>
-                                                    <input type="text" name="first_name" id="first_name"
+                                                    <input type="text" name="lastname" id="first_name"
                                                         class="form-control">
                                                 </td>
 
@@ -91,7 +95,7 @@
                                                 <td>Phone Number</td>
                                                 <td>:</td>
                                                 <td>
-                                                    <input type="number" name="jumlah_pesanan" id="jumlah_pesanan"
+                                                    <input type="number" name="phone_number" id="phone_number"
                                                         class="form-control">
 
                                                 </td>
@@ -100,7 +104,7 @@
                                                 <td>Post Code</td>
                                                 <td>:</td>
                                                 <td>
-                                                    <input type="number" name="jumlah_pesanan" id="jumlah_pesanan"
+                                                    <input type="number" name="post_code" id="post_code"
                                                         class="form-control">
 
                                                 </td>
@@ -109,7 +113,7 @@
                                                 <td>City</td>
                                                 <td>:</td>
                                                 <td>
-                                                    <input type="number" name="jumlah_pesanan" id="jumlah_pesanan"
+                                                    <input type="text" name="city" id="city"
                                                         class="form-control">
 
                                                 </td>
@@ -118,7 +122,7 @@
                                                 <td>Detail State</td>
                                                 <td>:</td>
                                                 <td>
-                                                    <input type="number" name="jumlah_pesanan" id="jumlah_pesanan"
+                                                    <input type="text" name="detail_state" id="detail_state"
                                                         class="form-control">
 
                                                 </td>
@@ -127,14 +131,14 @@
                                             <tr>
                                                 <td>
 
-                                                    <button type="submit" class="btn btn-primary mt-2 "><i
+                                                    <button type="submit" id="btn-pesan" class="btn btn-primary mt-2 "><i
                                                             class="fa fa-shopping-cart"></i>Pesan</button>
                                                 </td>
-                                    </form>
+                                </form>
 
 
 
-                                    </tbody>
+                                </tbody>
                                 </table>
                             </div>
                         </div>
@@ -143,10 +147,12 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
         integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
+        
         $(document).ready(function() {
             // Ambil UUID dari URL
             const uuid = window.location.href.split('/').pop();
@@ -166,11 +172,86 @@
                     $('#cpu').text(response.data.detail.cpu);
                     $('#baterry').text(response.data.detail.baterry);
                     $('#camera').text(response.data.detail.camera);
-                    
+
                 },
                 error: function(xhr, status, error) {
                     console.error(error);
                 }
+            });
+        });
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+
+        $(document).ready(function() {
+
+            var formTambah = $('#formTambah');
+            const uuid = window.location.href.split('/').pop();
+
+            // Set nilai product_id pada input field tersembunyi
+            $('#product_id').val(uuid);
+
+            formTambah.on('submit', function(e) {
+                e.preventDefault();
+                var formData = new FormData(this);
+                $.ajax({
+                    type: 'POST',
+                    url: '{{ url('v3/phone/order') }}',
+                    data: formData,
+                    dataType: 'json',
+                    contentType: false,
+                    processData: false,
+                    success: function(data) {
+                        if (data.message === 'check your validation') {
+                            var error = data.errors;
+                            var errorMessage = "";
+
+                            $.each(error, function(key, value) {
+                                errorMessage += value[0] + "<br>";
+                            });
+
+                            Swal.fire({
+                                title: 'Error',
+                                html: errorMessage,
+                                icon: 'error',
+                                timer: 5000,
+                                showConfirmButton: true
+                            });
+                        } else {
+                            console.log(data);
+                            Swal.fire({
+                                title: 'Success',
+                                text: 'Data Success Create',
+                                icon: 'success',
+                                showCancelButton: false,
+                                confirmButtonText: 'OK'
+                            }).then(function() {
+                                window.location.href =
+                                    "{{ url('/kuitansi/phone/') }}/" + data.data.id;
+                            });
+                        }
+                    },
+                    error: function(data) {
+                        var error = data.responseJSON.errors;
+                        var errorMessage = "";
+
+                        $.each(error, function(key, value) {
+                            errorMessage += value[0] + "<br>";
+                        });
+
+                        Swal.fire({
+                            title: 'Error',
+                            html: errorMessage,
+                            icon: 'error',
+                            timer: 5000,
+                            showConfirmButton: true
+                        });
+                    }
+                });
             });
         });
     </script>
