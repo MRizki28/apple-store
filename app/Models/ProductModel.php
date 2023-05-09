3ef30fefbb87399f76558a5dee4cea2502c04b63
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-09 22:19:49 +0700

    create function product

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\DetailModel;
+use App\Models\OrderanModel;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -15,6 +16,12 @@ class ProductModel extends Model
         'id', 'uuid', 'product_name', 'product_model', 'price', 'stock', 'detail_id', 'created_at', 'updated_at'
     ];
 
+    public function orderan()
+    {
+        return $this->hasMany(OrderanModel::class, 'product_id', 'id');
+    }
+
+
     public function detail()
     {
         return $this->belongsTo(DetailModel::class);
@@ -23,7 +30,7 @@ class ProductModel extends Model
     public function getProductDetail($detail_id)
     {
         $data = $this->join('tb_detail', 'tb_product.detail_id', '=', 'tb_detail.id')
-            ->select('tb_detail.uuid','tb_detail.image_phone','tb_detail.ram', 'tb_detail.storage', 'tb_detail.os', 'tb_detail.cpu', 'tb_detail.baterry', 'tb_detail.camera')
+            ->select('tb_detail.uuid', 'tb_detail.image_phone', 'tb_detail.ram', 'tb_detail.storage', 'tb_detail.os', 'tb_detail.cpu', 'tb_detail.baterry', 'tb_detail.camera')
             ->where('tb_product.detail_id', '=', $detail_id)
             ->first();
 
