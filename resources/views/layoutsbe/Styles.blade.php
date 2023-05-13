34279f0cf984a9209678a1844d73f9657643f0ee
Rizki <muhammadrizkitkj123@gmail.com>
2023-05-13 21:33:32 +0700

    update

--- resources/views/layoutsbe/Styles.blade.php
+++ resources/views/layoutsbe/Styles.blade.php
@@ -2,5 +2,5 @@
 <title>Apple Store</title>
 <link href="{{ asset('static/vendor/fontawesome-free/css/all.min.css ') }}" rel="stylesheet" type="text/css">
 <link href="{{ asset('static/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('static/css/ruang-admin.min.css') }}" rel="stylesheet">
+<link href="{{ asset('admin/css/ruang-admin.min.css') }}" rel="stylesheet">
 <link href="{{ asset('static/vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
